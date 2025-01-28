280be8d57840a713cded32f56035265f90b22153
steve <mariscurrenalae@gmail.com>
2025-01-28 11:49:59 -0600

    Adding the basic structure form

--- src/app.py
+++ src/app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from form import UserData
 import os
 
 #Allows to make the reference into the templates folder
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '..', 'templates'))
+app.config['SECRET_KEY'] = 'your_secret_key'
 
 @app.route('/')
-def form():
+def index():
     return render_template("index.html")
     #return "Prueba"
+
+@app.route('/form', methods=['GET','POST'])
+def form():
+    form = UserData()
+    return render_template('form.html', form=form)
 
 if __name__ == "__main__":
     #app.run(debug=True)
