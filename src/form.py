d40f727c9b6239ba22a60ca7282a3893055fce34
steve <mariscurrenalae@gmail.com>
2025-01-28 15:26:57 -0600

    Adding first style template to the form

--- src/form.py
+++ src/form.py
@@ -2,6 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 class UserData(FlaskForm):
-    user = StringField('Username')
-    pwd = StringField('Password')
+    user = StringField('Username  :')
+    pwd = StringField('Password    :')
     submit = SubmitField('Login')
